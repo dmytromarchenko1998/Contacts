06d2c4626b4819f94873eb619648cc99cca23cea
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-06-17 17:46:18 -0700

    retrieving data from graphql along with setting and retrieving cache

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,104 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { graphql, compose } from 'apollo-client';
 
-const QUERY = gql`
+const getAllContacts = gql`
   query {
     contacts {
+      id
       firstName
+      middleName
+      lastName
+      number
+      email
     }
   }
 `;
 
-const ContactList = () => (
-  <Query query={QUERY} >
+const getFormData = gql`
+  query {
+    currentForm @client {
+      firstName
+      middleName
+      lastName
+      number
+      email
+    }
+  }
+`;
+
+const updateForm = gql`
+  mutation updateForm($firstName: String!, $middleName: String!) {
+    updateForm(firstName: $firstName, middleName: $middleName) @client{
+      firstName
+    }
+  }
+`
+
+const App = () => (
+  <div className="App">
+    <Contacts />
+    <Form />
+    <FormDraft />
+  </div>
+)
+
+const Contacts = () => (
+  <Query query={getAllContacts} >
     {({ data, error, loading }) => {
       if (error) return '💩 Oops!';
       if (loading) return 'loading';
-      console.log(data);
-
       return (
-        <h1>{data.contacts[0].firstName}</h1>
+        data.contacts.map(({ firstName, middleName,lastName, number, email, id}) => (
+          <div id={id}>
+            <p>{`${firstName} ${middleName || ''} ${lastName}`}</p>
+            <p>{number}</p>
+            <p>{email}</p>
+          </div>
+        ))
       );
     }}
   </Query>
 );
 
-const App = () => <ContactList />;
+// const Form = () => (
+//   <Mutation mutation={updateForm}>
+//     {({ updateForm }, { data }) => (
+//     <div>
+//       <input onChange={({ target }) => {
+//         console.log(target.value);
+//         console.log(updateForm);
+//         updateForm({variables:{firstName:target.value, middleName: target.value}})}}
+//       />
+//     </div>
+//     )}
+//   </Mutation>
+// );
 
-export default App;
+// const FormDraft = () => (
+//   <Query query={getFormData}>
+//     {({ data, error, loading}) => {
+//       if (error) return '💩 Oops!';
+//       if (loading) return 'loading';
+//       console.log(data);
+//       return (
+//         <div>test</div>
+//       )
+//     }}
+//   </Query>
+// )
 
-
-// import React, { Component } from 'react';
-// import { graphql } from 'react-apollo'
-// import gql from 'graphql-tag'
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
+// const Form = () => {
+//   return (
+//     <div>
+//       <form>
+//         <input onChange={({ target }) => {console.log(target.value)}}/>
+//       </form>
+//     </div>
+//   )
 // }
 
-// export default App;
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,85 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, graphql} from 'react-apollo';
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { withClientState } from 'apollo-link-state';
+import { ApolloLink } from 'apollo-link';
+import gql from 'graphql-tag';
+import App from './App.jsx';
+
 
 const cache = new InMemoryCache();
 
-const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://192.168.29.248:8000/graphql'}),
-  cache: cache,
+const defaultState = {
+  currentForm: {
+    __typename: 'CurrentForm',
+    firstName: 'Dawg',
+    middleName: 'Dawg',
+    lastName: 'Dawg',
+    number: 'Dawg',
+    email:'Dawg',
+  }
+}
+
+const stateLink = withClientState({
+  cache,
+  defaults: defaultState,
+  resolvers: {
+    updateForm:(_, { firstName, middleName }, { cache }) => {
+      console.log(firstName, middleName);
+    }
+  }
 })
 
-const AppWithProvider = () => (
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    stateLink,
+    new HttpLink({
+      uri: 'http://192.168.29.248:8000/graphql'
+    })
+  ]),
+  cache
+})
+
+// const stateLink = withClientState({
+//   cache,
+//   resolvers: {
+//     Mutation: {
+//       updateNetworkStatus: (_, { isConnected }, { cache }) => {
+//         const data = {
+//           networkStatus: {
+//             __typename: 'NetworkStatus',
+//             isConnected
+//           },
+//         };
+//         cache.writeData({ data });
+//         return null;
+//       },
+//     },
+//   },
+//   defaults: {
+//     networkStatus: {
+//       __typename: 'NetworkStatus',
+//       isConnected: true,
+//     },
+//   },
+// });
+
+// const client = new ApolloClient({
+//   cache,
+//   link: ApolloLink.from([stateLink, new HttpLink({ uri: 'http://192.168.29.248:8000/graphql'})]),
+// });
+
+// const UPDATE_NETWORK_STATUS = gql`
+//   mutation updateNetworkStatus($isConnected: Boolean) {
+//     updateNetworkStatus(isConnected: $isConnected) @client
+//   }
+// `;
+
+const Root = () => (
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>
 );
 
-ReactDOM.render(<AppWithProvider />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
